7b039dd13df6f38de9c21a6ad4ff843f1f077650
Giuseppe Frattura <giuseppefrattura@gmail.com>
2023-05-10 18:14:08 +0200

    Finisced exercise 1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # nodejs-blog
-exercise from https://www.youtube.com/watch?v=-foo92lFIto
+exercise from 
+https://www.youtube.com/watch?v=-foo92lFIto&list=PL4cUxeGkcC9hAJ-ARcYq_z6lDZV7kT1xD
--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 require('dotenv').config();
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts')
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+//Template engine
+app.use(expressLayouts);
+app.set('layout','./layouts/main');
+app.set('view engine','ejs')
+
+
+app.use('/',require('./server/routes/main'));
+
+app.listen(PORT, ()=> {
+    console.log(`server is up on port ${PORT}`)
+
+})
